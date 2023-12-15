71c18bc4b4340301ce8924f3edd11454d5bb5fcb
mmkamal021 <114946934+mmkamal021@users.noreply.github.com>
2023-12-16 01:12:53 +0600

    Basic Event handler

---
+++ basic_eventHandler.js
@@ -0,0 +1,11 @@
+// Event handler
+
+function hello() {
+  console.log("Hello world");
+}
+button.addEventListener("click", hello);
+// Button is listing for user click event
+// This pattern is also named observe pattern
+
+// Select button
+const button = document.getElementById("button");
