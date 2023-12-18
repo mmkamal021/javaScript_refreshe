c8b9765a2444f924c8cd5dce4fb79fedf2a51a6b
mmkamal021 <114946934+mmkamal021@users.noreply.github.com>
2023-12-19 00:07:05 +0600

    Understanding basic concat method

---
+++ basic_concat.js
@@ -0,0 +1,10 @@
+// Array.prototype.concat()
+
+var numbers1 = [1, 2, 3, 4, 5];
+var numbers2 = [6, 7, 8, 9, 10];
+var numbers3 = [6, 7, 8, 9, 10];
+
+var result = numbers1.concat(numbers2, numbers3);
+
+console.log(numbers1);
+console.log(result);
