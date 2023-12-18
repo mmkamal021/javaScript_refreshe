ffa435555352bc769314feb5bc29a8d89bf151b8
mmkamal021 <114946934+mmkamal021@users.noreply.github.com>
2023-12-18 22:42:18 +0600

    Understanding basic Splice Method

---
+++ basic_slice.js
@@ -0,0 +1,10 @@
+// Array.prototype.slice()
+// There is no change main array here
+
+var numbers = [1, 2, 3, 4, 5];
+
+var result1 = numbers.slice(1, 3);
+var result2 = numbers.slice(-2, 4);
+
+console.log(result1);
+console.log(result2);
